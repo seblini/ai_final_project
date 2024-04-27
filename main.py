05cf7f1e26d8c5f6936984285cfdd1b571eaf68f
Sebastian Balderrama <capotillos11@gmail.com>
2024-04-26 21:06:05 -0600

    added car vision beams

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from utils import scale_image, blit_rotate_center
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 
-TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
-TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+mask_surface = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+mask = pygame.mask.from_surface(mask_surface)
 
 FINISH = pygame.image.load("imgs/finish.png")
 FINISH_MASK = pygame.mask.from_surface(FINISH)
@@ -21,6 +21,19 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Racing Game!")
 
 FPS = 60
+
+#####
+
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+beam_surface = pygame.Surface((500, 500), pygame.SRCALPHA)
+
+mask_fx = pygame.mask.from_surface(pygame.transform.flip(mask_surface, True, False))
+mask_fy = pygame.mask.from_surface(pygame.transform.flip(mask_surface, False, True))
+mask_fx_fy = pygame.mask.from_surface(pygame.transform.flip(mask_surface, True, True))
+flipped_masks = [[mask, mask_fy], [mask_fx, mask_fx_fy]]
 
 
 class AbstractCar:
@@ -109,17 +122,55 @@ def move_player(player_car):
     if not moved:
         player_car.reduce_speed()
 
+def draw_beam(surface, angle, pos):
+    c = math.cos(math.radians(angle))
+    s = math.sin(math.radians(angle))
+
+    flip_x = c < 0
+    flip_y = s < 0
+    filpped_mask = flipped_masks[flip_x][flip_y]
+    
+    # compute beam final point
+    x_dest = WIDTH * abs(c)
+    y_dest = HEIGHT * abs(s)
+
+    beam_surface.fill((0, 0, 0, 0))
+
+    # draw a single beam to the beam surface based on computed final point
+    pygame.draw.line(beam_surface, BLUE, (0, 0), (x_dest, y_dest))
+    beam_mask = pygame.mask.from_surface(beam_surface)
+
+    # find overlap between "global mask" and current beam mask
+    offset_x = WIDTH-1-pos[0] if flip_x else pos[0]
+    offset_y = HEIGHT-1-pos[1] if flip_y else pos[1]
+    hit = filpped_mask.overlap(beam_mask, (offset_x, offset_y))
+    if hit is not None and (hit[0] != pos[0] or hit[1] != pos[1]):
+        hx = WIDTH-1 - hit[0] if flip_x else hit[0]
+        hy = HEIGHT-1 - hit[1] if flip_y else hit[1]
+        hit_pos = (hx, hy)
+
+        pygame.draw.line(surface, BLUE, pos, hit_pos)
+        pygame.draw.circle(surface, GREEN, hit_pos, 3)
+
+    pygame.display.update()
+
 
 run = True
 clock = pygame.time.Clock()
 images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
-          (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
+          (FINISH, FINISH_POSITION), (mask_surface, (0, 0))]
+# images = [(mask_surface, (0, 0))]
 player_car = PlayerCar(8, 8)
 
 while run:
     clock.tick(FPS)
 
     draw(WIN, images, player_car)
+
+    beam_origin = player_car.x + RED_CAR.get_width() // 2, player_car.y + RED_CAR.get_height() // 2
+
+    for angle in range(0, 359, 30):
+        draw_beam(WIN, angle, beam_origin)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -128,7 +179,7 @@ while run:
 
     move_player(player_car)
 
-    if player_car.collide(TRACK_BORDER_MASK) != None:
+    if player_car.collide(mask) != None:
         player_car.bounce()
 
     finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
